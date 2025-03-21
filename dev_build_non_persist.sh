4c73d49f0918fb96c1ba63fe72c4024fd9fc6cdf
OliveRoquesUNCA <oliveroques78@gmail.com>
2025-03-20 23:23:01 -0400

    refactoring, abandoning python

--- dev_build_non_persist.sh
+++
@@ -1,5 +0,0 @@
-docker run \
-    --restart always \
-    --publish=7474:7474 --publish=7687:7687 \
-    --env NEO4J_AUTH=none \
-    neo4j
--- vite/src/server-requests.jsx
+++ vite/src/server-requests.jsx
@@ -11,6 +11,7 @@ export async function queryServer(query) {
     await driver.close();
     return;
   }
+
   let result = await driver.executeQuery(
     `${query}`,
     {},
